86d705291542a6efdf642f8ad525516699779a83
Minoua Guimera <minouaguimera@hotmail.com>
2024-04-25 12:48:35 -0300

    Adicionando salario e prompts ao programa - 2 aula

--- firstApp/script.js
+++ firstApp/script.js
@@ -1,11 +1,22 @@
 //input
-birthYear = 1999;
-name = 'Minoua'
+let birthYear = parseInt(prompt('When were you born ?'));
+let name = prompt('Insert your name.');
+let baseSalary = parseFloat(prompt('Insert your base salary'));
+let gratification = parseFloat(prompt('Insert how much is your gratification.'));
+let bonus = parseFloat(prompt('Insert how much is your bonus.'));
+let netSalary = 0
+let discount = parseFloat(prompt("How much you're discounted?"))
+let aditional = 0
+
 
 //processing
 today = new Date()
 actualYear = today.getFullYear()
 age = actualYear - birthYear
-msg = 'Im ' + name + ' and Im ' + age + ' years old.'
+netSalary = baseSalary + gratification + bonus - discount
 
-alert(msg)
+
+//output
+let message ='Im ' + name + ', Im ' + age + ' years old and my wage is ' + netSalary
+alert(message)
+
